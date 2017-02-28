dffd95e2aa3cfeba42750cc91720e49fa94fc726
lanlin <llan.xjtu@foxmail.com>
2017-02-28 14:39:08 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y install apt-transport-https ca-certificates
+sudo apt-get install -y --no-install-recommends \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+    
+sudo apt-get install -y --no-install-recommends \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
 
-sudo apt-key adv \
-               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
 
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
-
-sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
-
 sudo apt-get -y install docker-engine
 
 sudo groupadd docker
